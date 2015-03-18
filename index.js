da21d7743a0e22c921c793325285e2df485e95df
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-03-18 14:53:56 -0400

    Extract zip from POST

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
 app.get('/', function(request, response) {
   response.send('Hello World!!');
 });
 
 app.post('/', function(request, response) {
-  response.send('You just posted!!');
+  var zip = request.body.zip;
+  response.send('You just posted -- zip: ' + zip);
 });
 
 app.listen(app.get('port'), function() {
