bb9ad35e15f5f0c85c63e04ee461d0f53a187da9
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-03-18 13:24:59 -0400

    Hello world

---
+++ hello.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello world!');
+});
+
+// need to be root to run on port 80
+server.listen(8080);
+
