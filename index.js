3ccf0c2043b5c0bb15a38d0d631172a66fb6cd21
Anthony Elliott <anthony_elliott@ncsu.edu>
2015-03-18 14:17:25 -0400

    Post to /

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello World!!');
 });
 
+app.post('/', function(request, response) {
+  response.send('You just posted!!');
+});
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
